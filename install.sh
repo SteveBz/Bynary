40822399cd9e570fb0ed2d43745d4694a9c02d5e
Steve Cookson <steve.cookson@sca-uk.com>
2021-08-09 06:04:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,12 @@ mkdir eDR3
 cd eDR3
 cd ../..
 ## Install python modules:
-
+### Choose your wx
+#pip install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-8  wxPython
+pip install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-10  wxPython
+#pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-20.04 wxPython
 pip3 install numpy
 pip3 install matplotlib
-pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-20.04 wxPython
 pip3 install pandas
 pip3 install astroquery
 pip3 install dask
