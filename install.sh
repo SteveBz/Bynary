a0282a8b650fad186ecb6cdea350e22a50480992
Steve Cookson <steve.cookson@sca-uk.com>
2021-08-14 08:02:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ cd ../..
 ## Install python modules:
 ### Un comment your version of wx
 #pip install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-8  wxPython
-pip install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-10  wxPython
+pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-10  wxPython
 #pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-20.04 wxPython
 pip3 install numpy
 pip3 install matplotlib
