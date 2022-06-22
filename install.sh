365a6da0ea065b854165ed62394194ea31f3a54d
Steve Cookson <steve.cookson@sca-uk.com>
2022-06-22 17:06:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ cd ../..
 pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-10  wxPython
 #pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-20.04 wxPython
 pip3 install numpy
-pip3 install matplotlib
+pip3 install matplotlib==3.4.3
 pip3 install pandas
 #Next line needs rust
 pip3 install astroquery
