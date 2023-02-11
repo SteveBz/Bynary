81c0a18967dfc61306f3604a38037c5b78b6136e
Steve Cookson <steve.cookson@sca-uk.com>
2023-02-11 20:28:20 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ cd ../..
 ## Install python modules:
 ### Un comment your version of wx
 #pip install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-8  wxPython
-pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-10  wxPython
-#pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-20.04 wxPython
-pip3 install numpy
+#pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-10  wxPython
+pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-22.04 wxPython
+#pip3 install numpy
 pip3 install matplotlib==3.4.3
 pip3 install pandas
 #Next line needs rust
