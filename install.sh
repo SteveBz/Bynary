2fd4e4ed131693fce6a1260f772b92a8bfa654ce
Steve Cookson <steve.cookson@sca-uk.com>
2023-02-12 19:36:26 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,10 +11,12 @@ mkdir bindata
 #cd ..
 ## Install python modules:
 ### Un comment your version of wx
-#pip install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-8  wxPython
-#pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-10  wxPython
-#pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-20.04 wxPython
-pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-22.04 wxPython
+#pip install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-8  wxPython # debian-8
+#pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/debian-10  wxPython   # Linux debian-10
+#pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-20.04 wxPython  # Linux (K)ubuntu-20.04
+pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-22.04 wxPython  # Linux (K)ubuntu-22.04
+#pip install -U wxPython # Windows and macOS
+
 #pip3 install numpy
 pip3 install wheel
 pip3 install matplotlib==3.4.3
